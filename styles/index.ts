2dccbbf9590e9f4f71ba5e6a475b095595f13c6a
João Tupinambá <joaoh.tupinamba@gmail.com>
2023-01-21 19:12:42 -0300

    style main

---
+++ styles/index.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+  background-color: #1e1e1e;
+
+  button {
+    color: white;
+    cursor: pointer;
+    padding: 300px;
+    background-color: black;
+  }
+`;
