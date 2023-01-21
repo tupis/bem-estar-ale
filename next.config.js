b7bfec0e6685d1e9c7aaf99a8a243b3a3a6b460e
João Tupinambá <joaoh.tupinamba@gmail.com>
2023-01-21 10:11:43 -0300

    config: initial config styled components

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  compiler: {
+    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
+    styledComponents:
+      true |
+      {
+        ssr: true,
+        minify: true,
+      },
+    swcMinify: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
